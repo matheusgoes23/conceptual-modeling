0b08174045e7a9bdc64adbde2528c4bfdb64aa16
matheusgoes23 <matheusgoes23@gmail.com>
2022-01-17 11:12:04 -0300

    first commit

---
+++ src/main/java/com/matheusgoes/conceptual/modeling/ConceptualModelingApplication.java
@@ -0,0 +1,13 @@
+package com.matheusgoes.conceptual.modeling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptualModelingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConceptualModelingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matheusgoes/conceptual/modeling/ConceptualModelingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusgoes.conceptual.modeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConceptualModelingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
